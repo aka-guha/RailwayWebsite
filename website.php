87fce9edd83764c4b1b1f6b50c0e2e377a0e7fb3
aka-guha <60890529+aka-guha@users.noreply.github.com>
2020-02-26 21:52:15 +0530

    Add files via upload
    The main HTML code for the front end of the webpage.

---
+++ website.php
@@ -0,0 +1,220 @@
+<html>
+    <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Indian Railways</title>
+    <link href="webstyle.css" rel="stylesheet" type="text/css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+    </head>
+    <body>
+    <section id="nav-bar">
+        
+    <nav class="navbar navbar-expand-lg navbar-light">
+  <a class="navbar-brand" href="#"><img src="images/emblem.png"></a>
+        <div class="title">
+        <h1>INDIAN RAILWAYS</h1>
+        <h4>ALLAHABAD DIVISION, PERSONNEL BRANCH</h4>
+        </div>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarNav">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+      <div class="icon"><i class="fa fa-home"></i>
+          </div>  
+          <a class="nav-link" href="#slider">HOME</a>
+      </li>
+      <li class="nav-item">
+          <div class="icon">
+          <i class="fa fa-train" style="padding-left: 5px;"></i>
+        </div>
+        <a class="nav-link" href="https://enquiry.indianrail.gov.in/mntes/" target="new">ENQUIRY</a>
+      </li>
+      <li class="nav-item">
+          <div class="icon"><i class="fa fa-check-square-o" style="padding-left: 15px"></i>
+          </div>
+        <a class="nav-link" href="http://www.indianrail.gov.in/enquiry/PNR/PnrEnquiry.html?locale=en" target="new again">PNR STATUS</a>
+      </li>
+        <li class="nav-item dropdown">
+            <div class="icon"><i class="fa fa-file-text" style="padding-left: 30px; font-size: 36px; padding-top: 3px;  height: 40px;"></i>
+            </div>
+        <a class="nav-link dropdown-toggle" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">REGISTER HERE </a>
+        <div class="dropdown-menu">
+          <a class="dropdown-item" href="http://localhost/railway/loginsystem/" target="top">Account Sign-Up</a>
+          <a class="dropdown-item" href="form.php" target="top">Resgistration Form</a>
+        </div>
+      </li>
+        <li class="nav-item">
+            <div class="icon">
+            <i class="fa fa-envelope"></i>
+            </div>
+        <a class="nav-link" href="mailto:guhaakash007@gmail.com?Subject=Railway%20Topic" target="_top">CONTACT</a>
+        </li>
+    </ul>
+  </div>   <a class="navbar-brand" href="#"><img src="images/redlogo.png"></a>
+        </nav>
+        </section>
+        
+        <div id="slider">
+        <div id="headslider" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#headslider" data-slide-to="0" class="active"></li>
+    <li data-target="#headslider" data-slide-to="1"></li>
+    <li data-target="#headslider" data-slide-to="2"></li>
+      <li data-target="#headslider" data-slide-to="3"></li>
+      <li data-target="#headslider" data-slide-to="4"></li>
+      <li data-target="#headslider" data-slide-to="5"></li>
+      <li data-target="#headslider" data-slide-to="6"></li>
+      <li data-target="#headslider" data-slide-to="7"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+     <a href="https://en.wikipedia.org/wiki/Allahabad_Junction_railway_station" target="top" title="CLICK TO KNOW MORE ABOUT ALLAHABAD RAILWAY JUNCTION"><img src="images/alldstation2.jpg" class="d-block img-fluid"></a> 
+      </div>
+      
+    <div class="carousel-item">
+      <a href="https://en.wikipedia.org/wiki/History_of_rail_transport_in_India" target="top" title="CLICK TO MORE ABOUT HISTORY OF INDIAN RAILWAY"><img src="images/indianrail.jpg" class="d-block img-fluid"></a>
+      </div>
+      
+    <div class="carousel-item">
+      <img src="images/train.jpg" class="d-block img-fluid">
+      </div>
+       
+      <div class="carousel-item">
+      <img src="images/train5.jpg" class="d-block img-fluid">
+        </div>
+       
+      <div class="carousel-item">
+      <img src="images/train4.jpg" class="d-block img-fluid">
+      </div>
+     
+      <div class="carousel-item">
+      <img src="images/arttrain.jpg" class="d-block img-fluid"> 
+      </div>
+       
+      <div class="carousel-item">
+      <img src="images/metro.jpg" class="d-block img-fluid"> 
+      </div>
+      
+      <div class="carousel-item">
+      <a href="https://en.wikipedia.org/wiki/Vande_Bharat_Express" target="top" title="CLICK TO KNOW MORE ABOUT VANDE BHARAT"><img src="images/vbharat.png" class="d-block img-fluid"></a>
+      </div>
+      
+  </div>
+  <a class="carousel-control-prev" href="#headslider" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#headslider" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+</div> 
+        </div>
+        <section id="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6">
+                <h3 class="fs"> FILE MANAGEMENT SYSTEM</h3>
+                    <p> A filesystem is the methods and data structures that an operating system uses to keep track of files on a disk or partition; that is, the way the files are organized on the disk. The word is also used to refer to a partition or disk that is used to store the files or the type of the filesystem.</p>
+                    <button type="button" class="btn btn-primary"> Read More>></button>
+                    <div class="about-content">
+                    In computing, a file system or filesystem (often abbreviated to fs), controls how data is stored and retrieved. Without a file system, information placed in a storage medium would be one large body of data with no way to tell where one piece of information stops and the next begins. By separating the data into pieces and giving each piece a name, the information is easily isolated and identified. Taking its name from the way paper-based information systems are named, each group of data is called a "file". The structure and logic rules used to manage the groups of information and their names is called a "file system".
+                            <br>
+                    There are many different kinds of file systems. Each one has different structure and logic, properties of speed, flexibility, security, size and more. Some file systems have been designed to be used for specific applications. For example, the ISO 9660 file system is designed specifically for optical discs.
+                            <br>
+                    File systems can be used on numerous different types of storage devices that use different kinds of media. As of 2019, hard disk drives have been key storage devices and are projected to remain so for the foreseeable future. Other kinds of media that are used include SSDs, magnetic tapes, and optical discs. In some cases, such as with tmpfs, the computer's main memory (random-access memory, RAM) is used to create a temporary file system for short-term use.
+                            <br>
+                    Some file systems are used on local data storage devices; others provide file access via a network protocol (for example, NFS, SMB, or 9P clients). Some file systems are "virtual", meaning that the supplied "files" (called virtual files) are computed on request (such as procfs and sysfs) or are merely a mapping into a different file system used as a backing store. The file system manages access to both the content of files and the metadata about those files. It is responsible for arranging storage space; reliability, efficiency, and tuning with regard to the physical storage medium are important design considerations. 
+                    </div>
+                </div>
+                <div class="col-md-6">
+                <h3 class="ir">INDIAN RAILWAYS</h3>
+                     <p>Indian Railways is India's national railway system operated by the Ministry of Railways. It manages the fourth largest railway network in the world by size, with a route length of 67,368-kilometre and total track length of 121,407-kilometre as of March 2017.</p>
+                    <button type="button" class="btn btn-primary"> Read More>></button>
+                    <div class="about-content">
+                        The first railway proposals for India were made in Madras in 1832. The country's first train, Red Hill Railway (built by Arthur Cotton to transport granite for road-building), ran from Red Hills to the Chintadripet bridge in Madras in 1837. In 1845, the Godavari Dam Construction Railway was built by Cotton at Dowleswaram in Rajahmundry, to supply stone for the construction of a dam over the Godavari River. In 1851, the Solani Aqueduct Railway was built by Proby Cautley in Roorkee to transport construction materials for an aqueduct over the Solani River.
+                        <br>
+                        India's first passenger train, hauled by three steam locomotives (Sahib, Sindh and Sultan), ran for 34 kilometres (21 miles) with 400 people in 14 carriages on 1,676 mm (5 ft 6 in) broad gauge track between Bori Bunder (Mumbai) and Thane on 16 April 1853. The Thane viaducts, India's first railway bridges, were built over the Thane creek when the Mumbai-Thane line was extended to Kalyan in May 1854. Eastern India's first passenger train ran 24 miles (39 km) from Howrah, near Kolkata, to Hoogly on 15 August 1854. The first passenger train in South India ran 60 miles (97 km) from Royapuram- Veyasarapady (Madras) to Wallajah Road (Arcot) on 1 July 1856.
+                        <br>
+                        On 24 February 1873, a horse-drawn 3.8-kilometre (2.4 mi) tram opened in Calcutta between Sealdah and Armenian Ghat Street. On 9 May 1874, a horse-drawn tramway began operation in Bombay between Colaba and Parel. In 1897, lighting in passenger coaches was introduced by many railway companies. On 3 February 1925, the first electric passenger train in India ran between Victoria Terminus and Kurla.
+                        <br>    
+                        The organisation of Indian railways into regional zones began in 1951, when the Southern (14 April 1951), Central (5 November 1951) and Western (5 November 1951) zones were created. Fans and lights were mandated for all compartments in all passenger classes in 1951, and sleeping accommodations were introduced in coaches. In 1956, the first fully air-conditioned train was introduced between Howrah and Delhi. Ten years later, the first containerized freight service began between Mumbai and Ahmedabad. In 1986, computerized ticketing and reservations were introduced in New Delhi. 
+                    </div>
+                </div>
+            </div>
+            </div>
+        </section>
+        
+        <section id="officials">
+        <div class="container">
+            <h2>RAILWAY OFFICIALS</h2>
+            <div class="row">
+            <div class="col-md-6 profile-pic text-center">
+                <div class="img-box">
+                <img src="images/piyush.png" class="img-responsive">
+                    <ul>
+                        <a href="https://www.facebook.com/PiyushGoyalOffice/" target="top"><li><i class="fa fa-facebook"></i></li></a>
+                        <a href="https://twitter.com/PiyushGoyal?ref_src=twsrc%5Egoogle%7Ctwcamp%5Eserp%7Ctwgr%5Eauthor" target="top"><li><i class="fa fa-twitter"></i></li></a>
+                        <a href="https://in.linkedin.com/in/piyushgoyalofficial" target="top"><li><i class="fa fa-linkedin"></i></li></a>
+                    </ul>
+                </div>
+                <h5>Shri Piyush Goyal</h5>
+                <h6>Hon'ble Minister Of Railways</h6>
+                </div>
+                <div class="col-md-6 profile-pic text-center">
+                <div class="img-box">
+                <img src="images/suresh.jpg" class="img-responsive">
+                    <ul>
+                        <a href="https://www.facebook.com/SureshAngadi/" target="top"><li><i class="fa fa-facebook"></i></li></a>
+                        <a href="https://twitter.com/sureshangadi_?lang=en" target="top"><li><i class="fa fa-twitter"></i></li></a>
+                        <a href="" target="top"><li><i class="fa fa-linkedin"></i></li></a>
+                    </ul>
+                </div>
+                <h5>Shri Suresh Angadi</h5>
+                <h6>Hon'ble Minister Of State For Railways</h6>
+                </div>
+            </div>
+            </div>
+        </section>
+        <section id="footer">
+             <ul class="navigate">
+                    <li><a href="" style="color: white">Home</a></li> 
+                    <li><a href="http://localhost/railway/loginsystem/" target="top" style="color: white">Sign In</a></li> 
+                    <li><a href="form.php" target="new" style="color: white">Register Here</a></li>
+         </ul>
+            <div class="container text-right">
+            <i class="fa fa-copyright">&emsp;Copyright @ 2019 Centre For Railway Information Systems. All Rights Reserved</i>
+            </div>
+        </section>
+        
+        
+        
+        <script src="js/smooth-scroll.js"></script>
+        <script>
+	var scroll = new SmoothScroll('a[href*="#"]');
+        </script>
+        
+<script>
+var coll = document.getElementsByClassName("btn btn-primary");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+      var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+</script>
+        
+    </body>
+</html>
